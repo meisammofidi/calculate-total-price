747e70b7f31fbbfc2fa9fef899edfb3a82e5d6f1
Meisam Mofidi <meisam.mofidi@yahoo.com>
2021-04-24 19:42:52 -0400

    Created package json and data collection

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports = {
+    discounts: [
+        { orderValue: 1000, discountRate: 3 },
+        { orderValue: 5000, discountRate: 5 },
+        { orderValue: 7000, discountRate: 7 },
+        { orderValue: 10000, discountRate: 10 },
+    ],
+    taxRates: [
+        { province: 'AB', taxRate: 5 },
+        { province: 'ON', taxRate: 13 },
+        { province: 'QC', taxRate: 14.975 },
+        { province: 'MI', taxRate: 6 },
+        { province: 'DE', taxRate: 0 },
+    ]
+}
